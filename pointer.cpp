cc04d8305dc60e0f68bed88c5d52894699a53f6e
gowthami-123 <60397976+gowthami-123@users.noreply.github.com>
2020-01-29 17:58:48 +0530

    pointer file created

---
+++ pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int s=(*a)+(*b);
+    int d=(*a)-(*b)>0?(*a)-(*b):-(*a-*b);
+    *a=s;
+    *b=d;  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
